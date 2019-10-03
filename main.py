2a8a97239188ebce7a0078a7875f040188ed88b5
tejaswin <tnadahalli@ethz.ch>
2019-10-03 15:02:17 +0200

    Added UDP server

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from concurrent.futures import ThreadPoolExecutor
 from concurrent.futures import ProcessPoolExecutor
+from dnslib import DNSRecord
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from socketserver import BaseRequestHandler, UDPServer
 from ssl import wrap_socket
 
 import os
 
-class AcmeChallengeHandler(BaseHTTPRequestHandler):
+class AcmeDNSChallengeHandler(BaseRequestHandler):
+    def parse(self, data):
+        dns_request = DNSRecord.parse(data)
+        print(dns_request)
+        
+    def handle(self):
+        data = self.request[0].strip()
+        self.parse(data)
+        socket = self.request[1]
+        socket.sendto('Ack', self.client_address)
+
+class AcmeHTTPChallengeHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/html')
@@ -26,11 +39,10 @@ class ShutdownHandler(BaseHTTPRequestHandler):
         if (self.path == '/shutdown'):
             os.kill(os.getpid(), 9)
 
-def ShutdownHandlerCreator(servers):
-    return lambda *args: ShutdownHandler(servers, *args)
-
-with ThreadPoolExecutor(max_workers=3) as executor:
-    acme_challenge_server = HTTPServer(('', 5002), AcmeChallengeHandler)
+with ThreadPoolExecutor(max_workers=4) as executor:
+    acme_dns_challenge_server = UDPServer(('', 10053), AcmeDNSChallengeHandler)
+    
+    acme_http_challenge_server = HTTPServer(('', 5002), AcmeHTTPChallengeHandler)
 
     certificate_server = HTTPServer(('', 5001), CertificateHandler)
     certificate_server.socket = wrap_socket(certificate_server.socket,
@@ -39,6 +51,7 @@ with ThreadPoolExecutor(max_workers=3) as executor:
                                             server_side=True)
     shutdown_server = HTTPServer(('', 5003), ShutdownHandler)
 
-    executor.submit(acme_challenge_server.serve_forever)
+    executor.submit(acme_dns_challenge_server.serve_forever)
+    executor.submit(acme_http_challenge_server.serve_forever)
     executor.submit(certificate_server.serve_forever)
     executor.submit(shutdown_server.serve_forever)
