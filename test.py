b9c3a0cadd73c710c9c49c23a39d871db9bb231d
Tejaswi Nadahalli <nadahalli@gmail.com>
2019-11-06 15:09:14 +0100

    Moved away from PyCryptodome

--- test.py
+++ test.py
@@ -5,10 +5,14 @@ import struct
 import random
 import prettyprinter
 import time
+from binascii import hexlify, unhexlify
 prettyprinter.install_extras(['requests'])
-from Cryptodome.Hash import SHA256
-from Cryptodome.PublicKey import ECC
-from Cryptodome.Signature import DSS
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.asymmetric.utils import decode_dss_signature
 
 
 def encode_json_as_bytes(j):
@@ -30,19 +34,44 @@ def get_nonce():
     )
     return r.headers['Replay-Nonce']
 
+def get_large_int_as_bytes(i, size):
+    padding = ((size + 7) // 8) * 2
+    hi = hex(i).rstrip("L").lstrip("0x")
+    length = len(hi)
+    if padding > length:
+        padding -= length
+    else:
+        padding = length % 2
+    return unhexlify(padding * '0' + hi)
+
+def get_large_int_as_base64(i, size):
+    return base64_encode_as_string(get_large_int_as_bytes(i, size))
+
 def get_jwk(private_key):
-    public_key = private_key.public_key()
+    private_numbers = private_key.private_numbers()
+    public_numbers = private_numbers.public_numbers
+    size = public_numbers.curve.key_size
     jwk = {}
     jwk['kty'] = 'EC'
     jwk['crv'] = 'P-256'
-    jwk['x'] = base64_encode_as_string(public_key.pointQ.x.to_bytes())
-    jwk['y'] = base64_encode_as_string(public_key.pointQ.y.to_bytes())
+    jwk['x'] = get_large_int_as_base64(public_numbers.x, size)
+    jwk['y'] = get_large_int_as_base64(public_numbers.y, size)
     return jwk
 
+def sign(private_key, data):
+    signature = decode_dss_signature(private_key.sign(data, ec.ECDSA(hashes.SHA256())))
+    private_numbers = private_key.private_numbers()
+    public_numbers = private_numbers.public_numbers
+    size = public_numbers.curve.key_size
+    return base64_encode_as_string(get_large_int_as_bytes(signature[0], size) + get_large_int_as_bytes(signature[1], size))
+
+def hash_256(data_as_bytes):
+    digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
+    digest.update(data_as_bytes)
+    return digest.finalize()
+
 def get_jwk_thumbprint(private_key):
-    jwk = get_jwk(private_key)
-    h = SHA256.new(encode_json_as_bytes(jwk))
-    return base64_encode_as_bytes(h.digest())
+    return base64_encode_as_bytes(hash_256(encode_json_as_bytes(get_jwk(private_key))))
 
 def get_jws_params(private_key, url, kid=None):
     jws_params = {}
@@ -67,10 +96,7 @@ def jws_encode_sign(private_key, payload_dict, jws_params):
     else:
         to_be_signed += b''
 
-    h = SHA256.new(to_be_signed)
-    signer = DSS.new(private_key, 'fips-186-3')
-    signature = signer.sign(h)
-    return base64_encode_as_string(signature)
+    return sign(private_key, to_be_signed)
 
 def make_request(private_key, account, url, payload):
     request_headers = {}
@@ -110,6 +136,7 @@ def new_account(private_key):
 
     prettyprinter.pprint(headers)
     prettyprinter.pprint(response)
+
     return headers['Location']
 
 def new_order(private_key, account):
@@ -129,7 +156,6 @@ def auths(private_key, account, auths):
     result = []
     for url in auths:
         _, response = make_request(private_key, account, url, payload)
-        prettyprinter.pprint(response)
         result.extend(response['challenges'])
     return result
 
@@ -139,13 +165,25 @@ def prompt_challenge(private_key, account, url):
 
 def do_dns(private_key, challenge):
     to_be_hashed = bytes(challenge['token'], 'utf-8') + b'.' + get_jwk_thumbprint(private_key)
-    h = SHA256.new(to_be_hashed)
-    txt_record = base64_encode_as_string(h.digest())
+    txt_record = base64_encode_as_string(hash_256(to_be_hashed))
     f = open('x/_acme-challenge.example.org', 'w')
     f.write(txt_record)
     f.close()
 
-private_key = ECC.generate(curve='P-256')
+def do_dns_challenge(private_key, account, order):
+    challenges = auths(private_key, account, order['authorizations'])
+    for c in challenges:
+        if c['type'] == 'dns-01':
+            do_dns(private_key, c)
+
+    for c in challenges:
+        if c['type'] == 'dns-01':
+            prompt_challenge(private_key, account, c['url'])
+
+    time.sleep(2)
+
+    
+private_key = ec.generate_private_key(ec.SECP256R1(), default_backend())
 account = new_account(private_key)
 order = new_order(private_key, account)
 prettyprinter.pprint(order)
@@ -158,6 +196,14 @@ for c in challenges:
     if c['type'] == 'dns-01':
         prompt_challenge(private_key, account, c['url'])
 
+"""
+
+account = new_account(private_key)
+order = new_order(private_key, account)
+do_dns_challenge_and_finalize(private_key, account, order)
+
+prettyprinter.pprint(order)
+"""
         
 
     
